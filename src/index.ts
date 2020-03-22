4210a648b01b14038dbf2e630869517a2ea0f0ae
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-03-22 15:40:30 -0300

    update index with model

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { IEntitySearch, IEnumerationRelated, IRelatedIds, IProperty, Sort, IResult } from "./model/model";
-
+import { SearchType, TypeEntity} from "./model/SearchType";
+ 
 export {
     IEntitySearch,
     IEnumerationRelated,
     IRelatedIds,
     IProperty,
     IResult,
-    Sort
+    Sort,
+    SearchType,
+    TypeEntity
 }
