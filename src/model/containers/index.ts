7c6a8e47b5f60502340ab619b81fcc71b196ca05
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-07-18 21:48:55 -0400

    update model from mdm-gen

---
+++ src/model/containers/index.ts
@@ -0,0 +1,20 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+export * from './CollectionResult';
+export * from './Facet';
+export * from './GroupInput';
+export * from './GroupMenu';
+export * from './IBoolProperty';
+export * from './IDblProperty';
+export * from './IDtProperty';
+export * from './IEntitySearch';
+export * from './IEnumProperty';
+export * from './INum32Property';
+export * from './INum64Property';
+export * from './IRelatedId';
+export * from './IStrProperty';
+export * from './KindProperty';
+export * from './OrderItem';
