9751059b3c95e7b49847aeb2372fd2f0aa2c3913
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-07-19 16:52:59 -0400

    update model from mdm-gen

---
+++ src/model/main/index.ts
@@ -0,0 +1,19 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+export * from './BoolProperty';
+export * from './DblProperty';
+export * from './DtProperty';
+export * from './EntitySearch';
+export * from './EnumProperty';
+export * from './GeographyProperty';
+export * from './GeoPointTs';
+export * from './IProperty';
+export * from './IPropertyFaceTable';
+export * from './Num32Property';
+export * from './Num64Property';
+export * from './PropertyBaseFaceTable';
+export * from './RelatedId';
+export * from './StrProperty';
