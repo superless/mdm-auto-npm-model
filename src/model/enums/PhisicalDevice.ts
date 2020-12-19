077f7e860266daa2a34e9b76e4e955292b7c730f
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-18 21:34:31 -0300

    Generando modelo

---
+++ src/model/enums/PhisicalDevice.ts
@@ -0,0 +1,10 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+export enum PhisicalDevice {
+    WEB = 0,
+    MOBILE = 1,
+    ALL = 2,
+}
---
+++ src/model/main/index.ts
@@ -0,0 +1,27 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+export * from './BoolProperty';
+export * from './DblProperty';
+export * from './DtProperty';
+export * from './EntityBaseSearch';
+export * from './EnumProperty';
+export * from './GeographyProperty';
+export * from './GeoPointTs';
+export * from './IBoolProperty';
+export * from './IDblProperty';
+export * from './IDtProperty';
+export * from './IEnumProperty';
+export * from './INum32Property';
+export * from './INum64Property';
+export * from './IProperty';
+export * from './IPropertyFaceTable';
+export * from './IRelatedId';
+export * from './IStrProperty';
+export * from './Num32Property';
+export * from './Num64Property';
+export * from './PropertyBaseFaceTable';
+export * from './RelatedId';
+export * from './StrProperty';
