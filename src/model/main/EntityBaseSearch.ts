d716574b9423b538389e1536c00c9f20089b1f9d
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-18 21:03:38 -0300

    Generando modelo

---
+++ src/model/main/EntityBaseSearch.ts
@@ -0,0 +1,33 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { IBoolProperty } from './IBoolProperty';
+import { IDblProperty } from './IDblProperty';
+import { IDtProperty } from './IDtProperty';
+import { IEnumProperty } from './IEnumProperty';
+import { INum32Property } from './INum32Property';
+import { INum64Property } from './INum64Property';
+import { IRelatedId } from './IRelatedId';
+import { IStrProperty } from './IStrProperty';
+import { IProperty } from './IProperty';
+import { GeoPointTs } from './GeoPointTs';
+
+export interface EntityBaseSearch<GeoPointTs> {
+    id: string;
+    created: Date;
+    index: number;
+    bl: IBoolProperty[];
+    dbl: IDblProperty[];
+    dt: IDtProperty[];
+    enm: IEnumProperty[];
+    num32: INum32Property[];
+    num64: INum64Property[];
+    rel: IRelatedId[];
+    str: IStrProperty[];
+    sug: IStrProperty[];
+    hm: string;
+    hh: string;
+    geo: IProperty<GeoPointTs>[];
+}
