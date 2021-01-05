530f98e338ffb6a02b1911c2a767c555154e940f
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2021-01-05 16:56:09 +0000

    +semver: patch

---
+++ src/model/containers/GroupInput.ts
@@ -0,0 +1,13 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { PhisicalDevice } from '../enums/PhisicalDevice';
+
+export interface GroupInput {
+    orderIndex?: number;
+    title: string;
+    device?: PhisicalDevice;
+    columnProportion?: number;
+}
---
+++ src/model/main/DblProperty.ts
@@ -0,0 +1,9 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+export interface DblProperty {
+    index: number;
+    value: number;
+}
