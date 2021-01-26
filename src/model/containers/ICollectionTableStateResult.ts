57ac5b3be250c1fb6390944c9ed1ebdb603582bf
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2021-01-26 22:47:42 +0000

    +semver: patch

---
+++ src/model/containers/ICollectionTableStateResult.ts
@@ -0,0 +1,10 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { ICollectionResult<T>, ICollectionTableState<T> } from './';
+
+export interface ICollectionTableStateResult<T> extends ICollectionResult<T>, ICollectionTableState<T> {
+
+}
