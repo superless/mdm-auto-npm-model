3ed68ffcf8de910ffe0111b86c1e950ba721e61b
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2021-02-23 22:34:23 +0000

    +semver: patch

---
+++ src/data/index.ts
@@ -0,0 +1,12 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+export * from './EntityKind';
+export * from './EntityMetadata';
+export * from './EntitySearchDisplayInfo';
+export * from './ModelMetaData';
+export * from './PropertyMetadadataEnum';
+export * from './PropertyMetadata';
+export * from './RelatedPropertyMetadata';
---
+++ src/model/main/GeographyProperty.ts
@@ -0,0 +1,11 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { GeoPointTs } from './GeoPointTs';
+
+export interface GeographyProperty {
+    index: number;
+    value: GeoPointTs;
+}
