2143cfc3bf1447f934874441d8b5a19d3cd4072d
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2021-03-08 19:00:44 +0000

    Generando modelo de datos

---
+++ src/data/InputPropRelatedDetails.ts
@@ -0,0 +1,11 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { RelatedPropertyMetadata } from './RelatedPropertyMetadata';
+
+export interface InputPropRelatedDetails extends RelatedPropertyMetadata {
+    modelPropName: string;
+    isReference: boolean;
+}
---
+++ src/data/RelatedItem.ts
@@ -0,0 +1,12 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { ToProcessClass } from './ToProcessClass';
+
+export interface RelatedItem {
+    className: string;
+    index: number;
+    pathToEntity: ToProcessClass;
+}
